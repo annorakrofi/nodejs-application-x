37046811720c9af915b313e737c3c9143acf2f6f
annorakrofi <53880172+annorakrofi@users.noreply.github.com>
2025-03-06 09:51:16 +0000

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://localhost:9000/',
+  serverUrl: 'http://172.31.16.31:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': 'c17c6a3302b6604269ba0bcc0031e674b2f488b2',
+	    'sonar.login': 'sqa_879a2317edf06e8e4a5847951ce6e21be75b729b',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
